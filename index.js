d9bbb37b7bb2377e52a9d9920694800b8b65101e
heeeun <heeeun12@bizpeer.co.kr>
2024-11-19 15:55:23 +0900

    User Init & Sign In

--- index.js
+++ index.js
@@ -3,13 +3,39 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+const{User} = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/test').then(() => console.log('MongoDB Connected..'))
+mongoose.connect('mongodb://localhost:27017/node',{
+    useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected..'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        return res.status(200).json({
+            success: true
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            err: err.message || 'Server Error'
+        })
+    }
 })
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
         type: String,
-        maxlength:50
+        maxlength: 50
     },
     email: {
         type: String,
-        trim:true,
+        trim: true,
         unique: 1
     },
     password: {
@@ -18,7 +20,7 @@ const userSchema = mongoose.Schema({
         type: String,
         maxlength: 50
     },
-    role :{
+    role: {
         type: Number,
         default: 0
     },
@@ -26,11 +28,27 @@ const userSchema = mongoose.Schema({
     token: {
         type: String
     },
-    tokenExp : {
+    tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function (next) {
+    var user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
     }
 })
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = {User}
+module.exports = { User }
